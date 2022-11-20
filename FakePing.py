98602ebbb4d474b77de1dcca5a6518066f94f836
Ijidishurka <108403538+Ijidishurka@users.noreply.github.com>
2022-11-20 11:16:05 +0200

    Update FakePing.py

--- FakePing.py
+++ FakePing.py
@@ -1,3 +1,4 @@
+import time
 import random
 import logging
 from .. import loader, utils
@@ -13,18 +14,29 @@ def register(cb):
 
 class fake_ping(loader.Module):
     """Подпишись на канал @modwini"""
-    strings = {'name': 'fake ping v3'}
+    strings = {'name': 'fake ping v4'}
 
     async def pinjcmd(self, message):
         """Используй .pinj <цифры>."""
         text = utils.get_args_raw(message)
         if not text:
-            await message.edit("⏱" + "<b> Скорость отклика Telegram:\n </b>" + f"<code>{random.randint(10, 600)}.{random.randint(10, 99)}</code>" + "<b> ms</b>" + " <b> \n😎 Прошло с последней  перезагрузки: </b>" + f" <b>{random.randint(10, 23)}</b>" + ":" + f"<b>{random.randint(10, 59)}</b>" + ":" + f"<b>{random.randint(10, 59)}</b>")
+            for pinj in ["<code>🐻 Nofin...</code>"]:
+                await message.edit(pinj)
+                await sleep(0.3)
+
+            named_tuple = time.localtime()
+            time_string = time.strftime("%H:%M:%S", named_tuple)
+
+            await message.edit(f"<b>⏱ Скорость отклика Telegram:</b>\n<code>{random.randint(10, 1999)}.{random.randint(10, 99)}</code> <b>ms</b>\n<b>😎 Прошло с последней перезагрузки: {time_string}</b>")
             return
         else:
             for pinj in ["<code>🐻 Nofin...</code>"]:
                 await message.edit(pinj)
                 await sleep(0.3)
-            pinj = ("⏱" + "<b> Скорость отклика Telegram:\n </b>" + f"<code>{text}</code>" + "<b> ms</b>" + " <b> \n😎 Прошло с последней  перезагрузки: </b>" + f" <b>{random.randint(10, 23)}</b>" + ":" + f"<b>{random.randint(10, 59)}</b>" + ":" + f"<b>{random.randint(10, 59)}</b>")
+
+            named_tuple = time.localtime()
+            time_string = time.strftime("%H:%M:%S", named_tuple)
+            
+            pinj = (f"<b>⏱ Скорость отклика Telegram:</b>\n<code>{text}</code> <b>ms</b>\n<b>😎 Прошло с последней перезагрузки: {time_string}</b>")
 
             await message.edit(pinj)
